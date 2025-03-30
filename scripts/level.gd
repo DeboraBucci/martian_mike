8a1a9a27f2e6dddf74b3b760016a3a45ffd3c2f8
Debora Bucci <dbucci@protonmail.com>
2025-03-30 19:09:16 -0300

    in the level script, use the _process function for handling the quitting and resetting of the game

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
